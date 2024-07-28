d5e939c54800871baca768f6e31fbe331c6ee7e8
Niall1985 <dcunhaniall@gmail.com>
2024-07-29 01:57:50 +0530

    used pyspellchecker to correct spellings present in the review

--- Sentiment_analyzer.py
+++ Sentiment_analyzer.py
@@ -1,19 +1,33 @@
 import spacy
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from spellchecker import SpellChecker
+
 
 load_english_model = spacy.load('en_core_web_sm')
 
+spell = SpellChecker()
+
+def correct_spelling(word):
+    return spell.candidates(word).pop() if spell.candidates(word) else word
+
+def preprocess_text(text):
+    words = text.split()
+    corrected_words = [correct_spelling(word) for word in words]
+    return ' '.join(corrected_words)
+
 def analyze_sentiment(review_to_be_analyzed):
     doc = load_english_model(review_to_be_analyzed)
-    # print(doc)
-
     analyzer = SentimentIntensityAnalyzer()
     sentiment_score = analyzer.polarity_scores(review_to_be_analyzed)
-
     return sentiment_score
 
 review_to_be_analyzed = input("Enter the review to be analyzed here:")
-sentiment_score = analyze_sentiment(review_to_be_analyzed)
+
+preprocessed_review = preprocess_text(review_to_be_analyzed)
+
+print(f"Corrected Sentence: {preprocessed_review}")
+
+sentiment_score = analyze_sentiment(preprocessed_review)
 print(f"Sentiment Score: {sentiment_score}")
 
 if sentiment_score['compound'] >= 0.31:
