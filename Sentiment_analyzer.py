7487f16f7aab59b50886977d4694a146e592fa10
Niall1985 <dcunhaniall@gmail.com>
2024-07-29 01:31:24 +0530

    minor changes

--- Sentiment_analyzer.py
+++ Sentiment_analyzer.py
@@ -1,15 +1,13 @@
 import spacy
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-load_english_model = spacy.load('en_core_web_sm') #loads the spacy english model
+load_english_model = spacy.load('en_core_web_sm')
 
 def analyze_sentiment(review_to_be_analyzed):
     doc = load_english_model(review_to_be_analyzed)
     # print(doc)
 
-    analyzer = SentimentIntensityAnalyzer() #initializes the VADER sentiment analyzer
-
-    # Calculate sentiment scores
+    analyzer = SentimentIntensityAnalyzer()
     sentiment_score = analyzer.polarity_scores(review_to_be_analyzed)
 
     return sentiment_score
@@ -18,7 +16,6 @@ review_to_be_analyzed = input("Enter the review to be analyzed here:")
 sentiment_score = analyze_sentiment(review_to_be_analyzed)
 print(f"Sentiment Score: {sentiment_score}")
 
-#compound score calculation 
 if sentiment_score['compound'] >= 0.31:
     print("Positive sentiment 😁")
 elif sentiment_score['compound'] <= -0.01:
